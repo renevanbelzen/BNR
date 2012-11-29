75765ed1ba22ca321431880cb53bd5a432c59315
Rene van Belzen <rene.van.belzen@gmail.com>
2012-11-29 14:51:30 +0100

    initial commit

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Rene van Belzen on 10/27/12.
+//  Copyright (c) 2012 Rene van Belzen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+     Maybe it needs a rewrite. */
+    
+    return 0;
+}
