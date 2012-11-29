421fa65c84465a4a2a3e5fc44b7578f61605e9ae
Rene van Belzen <rene.van.belzen@gmail.com>
2012-11-29 15:09:54 +0100

    ClassCertificates initial code

---
+++ ClassCertificates/ClassCertificates/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  ClassCertificates
+//
+//  Created by Rene van Belzen on 11/29/12.
+//  Copyright (c) 2012 Rene van Belzen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("Mark has done as much Cocoa Programming as I could fit into 5 days\n");
+    printf("Bo has done as much Objective-C Programming as I could fit into 2 days\n");
+    printf("Mike has done as much Python Programming as I could fit into 5 days\n");
+    printf("Ted has done as much iOS Programming as I could fit into 5 days\n");
+    
+    return 0;
+}
+
