f09f36c5ae339fe932bf62dbbe7e2f27d677278d
Rene van Belzen <rene.van.belzen@gmail.com>
2012-11-29 15:18:02 +0100

    ClassCertificates: added congratulateStudent().

--- ClassCertificates/ClassCertificates/main.c
+++ ClassCertificates/ClassCertificates/main.c
@@ -8,12 +8,18 @@
 
 #include <stdio.h>
 
+void congratulateStudent(char *student, char *course, int numDays)
+{
+    printf("%s has done as much %s Programming as I could fit into %d days.\n",
+           student, course, numDays);
+}
+
 int main(int argc, const char * argv[])
 {
-    printf("Mark has done as much Cocoa Programming as I could fit into 5 days\n");
-    printf("Bo has done as much Objective-C Programming as I could fit into 2 days\n");
-    printf("Mike has done as much Python Programming as I could fit into 5 days\n");
-    printf("Ted has done as much iOS Programming as I could fit into 5 days\n");
+    congratulateStudent("Mark", "Cocoa", 5);
+    congratulateStudent("Bo", "Objective-C", 2);
+    congratulateStudent("Mike", "Python", 5);
+    congratulateStudent("Ted", "iOS", 5);
     
     return 0;
 }
